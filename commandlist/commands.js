7b88ead15cb6b3d2d07cd176d53f970f1cc31574
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-06-28 15:13:04 +0900

    Create commands.js

---
+++ commandlist/commands.js
@@ -0,0 +1,22 @@
+function openModal(modalId) {
+  document.getElementById(modalId).style.display = "block";
+  document.getElementById(modalId).classList.add("show");
+}
+
+function closeModal(modalId) {
+  document.getElementById(modalId).style.display = "none";
+  document.getElementById(modalId).classList.remove("show");
+}
+
+window.addEventListener('scroll', function() {
+  var backToTop = document.getElementById('back-to-top');
+  if (window.scrollY > 100) {
+      backToTop.classList.add('show');
+  } else {
+      backToTop.classList.remove('show');
+  }
+});
+
+document.getElementById('back-to-top').addEventListener('click', function() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
