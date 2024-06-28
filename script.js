2353aa47221aeb059bf249597d4ab00182ba45e2
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-06-28 15:09:40 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
-function openModal(modalId) {
-  document.getElementById(modalId).style.display = "block";
-  document.getElementById(modalId).classList.add("show");
-}
-
-function closeModal(modalId) {
-  document.getElementById(modalId).style.display = "none";
-  document.getElementById(modalId).classList.remove("show");
-}
-
 window.addEventListener('scroll', function() {
   var backToTop = document.getElementById('back-to-top');
-  if (window.pageYOffset > 100) {
+  if (window.scrollY > 100) {
       backToTop.classList.add('show');
   } else {
       backToTop.classList.remove('show');
@@ -20,3 +10,4 @@ window.addEventListener('scroll', function() {
 document.getElementById('back-to-top').addEventListener('click', function() {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 });
+
