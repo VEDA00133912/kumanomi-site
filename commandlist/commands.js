26507ab3c020e6ecc66dd4915db20d9a909682b2
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-12-12 12:22:18 +0900

    Update commands.js

--- commandlist/commands.js
+++ commandlist/commands.js
@@ -20,3 +20,18 @@ window.addEventListener('scroll', function() {
 document.getElementById('back-to-top').addEventListener('click', function() {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 });
+
+document.getElementById('status-link').addEventListener('click', function(event) {
+    event.preventDefault();
+    const statusCard = document.getElementById('status-card');
+    statusCard.classList.add('show');
+});
+
+document.getElementById('confirm-status').addEventListener('click', function() {
+    window.open('https://kumanomi.instatus.com/', '_blank');
+    document.getElementById('status-card').classList.remove('show');
+});
+
+document.getElementById('cancel-status').addEventListener('click', function() {
+    document.getElementById('status-card').classList.remove('show');
+});
